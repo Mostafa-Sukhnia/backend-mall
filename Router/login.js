4add26873d6c3cecf0cfddb1009d09b25a5aee82
Mustafa-Sukhnia <mustafa.2005.sukhnia@gmail.com>
2025-11-23 15:30:15 +0300

    feat: implement JWT authentication with token generation and cookie handling

--- Router/login.js
+++ Router/login.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-
-const loginRouter = Router();
+import {generateToken, verifyToken} from "../lib/auth.js";
+export const loginRouter = Router();
 
 loginRouter.get("/", (_req, res) => {
   res.json({ message: "Login route working (GET)" });
@@ -9,11 +9,17 @@ loginRouter.get("/", (_req, res) => {
 loginRouter.post("/", (req, res) => {
   const { email, password } = req.body;
 
+  const token = generateToken({ userId: 1, username: "mustafa" });
+
+  res.cookie("auth_token", token, {
+    httpOnly: true,       
+    secure: false,        
+    sameSite: "lax",      
+    maxAge: 60 * 60 * 1000 
+  });
+
   res.json({
     success: true,
     message: "Login data received",
-    data: { email, password },
   });
 });
-
-export default loginRouter;
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { PORT } from "./config/env.js";
-import loginRouter from "./Router/login.js";
+import {loginRouter} from "./Router/login.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
+dotenv.config();
 const app = express();
-
+app.use(cookieParser());
 app.use(express.json());
-
 app.use("/api/login", loginRouter);
 
 app.get("/", (req, res) => {
